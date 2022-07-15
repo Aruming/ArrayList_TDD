1b273a054b8cc12aa9011031500ba3d82636a2c8
Aruming <0430kar@naver.com>
2022-07-15 10:58:21 +0900

    ArrayList에 값을 추가하는 add 메서드

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -1,10 +1,38 @@
 public class ArrayList {
     int size;
+    int[] data;
+    int lastIndex;
 
     public ArrayList(){
         size = 0;
+        data = new int[2];
     }
     public int size() {
         return size;
+    }
+
+    public void add(int i) {
+        sizeUpIfFull();
+        
+        data[size] = i;
+        size++;
+    }
+
+    private void sizeUpIfFull() {
+        if(isFull())
+            sizeUP();
+    }
+
+    private void sizeUP() {
+        int[] newData = new int[data.length*2];
+
+        for(int i=0;i<data.length;i++){
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    private boolean isFull() {
+        return data.length==size;
     }
 }
